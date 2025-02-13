a8d0297057afda6d5cbd7f3891ff1a3a8fb7d6da
CodeCrafter <roktimbailung100@gmail.com>
2025-02-13 21:04:33 +0530

    Create 2nd.py

---
+++ 2nd.py
@@ -0,0 +1,8 @@
+# Using Basic Print and Input command to take input from User and Displaying some Results
+print("Type your name")
+n= str(input())
+print("Type your location")
+p= str(input())
+print(" hello ", n, " how is the weather in",p,"?")
+age= int(input("What is your age?"))
+print("Good to know that you are",age,"years old")
