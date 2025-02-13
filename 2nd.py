ae46324ee0a72ed1060e942db551e3f834e6f9b1
CodeCrafter <roktimbailung100@gmail.com>
2025-02-13 21:22:47 +0530

    Update 2nd.py

--- 2nd.py
+++ 2nd.py
@@ -6,3 +6,13 @@ p= str(input())
 print(" hello ", n, " how is the weather in",p,"?")
 age= int(input("What is your age?"))
 print("Good to know that you are",age,"years old")
+
+# Calculating Area of Different Geometrical Figures
+rad=int(input("enter the radius of the circle"))
+area=3.14*rad*rad
+print("area of the circle with radius",rad,"is",area)
+
+l=float(input("Enter the length \n"))
+b=float(input("Enter the breadth \n"))
+area1=l*b
+print("The area of the Rectangle is",area1)
