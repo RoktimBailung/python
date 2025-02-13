92e518d336cacc1c9ac110965821370060613ceb
CodeCrafter <roktimbailung100@gmail.com>
2025-02-13 22:41:03 +0530

    Create 2nd.py
    About different types of Operators and their uses.

---
+++ 2nd.py
@@ -0,0 +1,38 @@
+# Operators,Union,Concatenation,precedence
+
+a=90
+b=100
+c=a+b
+print(c)
+
+a='roktim'
+b='bailung'
+c=a+b
+print(c)
+
+a=10+8*2
+print(a)
+b=(10+8)*2
+print(b)
+
+# ARITHMETIC OPRATOR
+print('addition',2+3)
+print('subtraction',9-1)
+print('multiplication',3*7)
+print('division',4/3)
+print('floor divsion',4//3)
+print('modulus',7%3)
+print('exponential',6**2)
+
+# RELATIONAL OPERATOR
+print(5>10)
+print(10<5)
+print(10!=10)
+print(10==10)
+
+# LOGICAL OPERATOR
+print(True and True)
+print(True and False)
+print(False and True)
+print((10>2)or(4<1))
+print(not(10))
